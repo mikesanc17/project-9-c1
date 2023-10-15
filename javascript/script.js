3634591950f5b0f30dd778b36e136cbbf9c02682
Mike Sanchez <sanchezmike017@gmail.com>
2023-10-14 23:08:57 -0700

    pushing project 9

---
+++ javascript/script.js
@@ -0,0 +1,13 @@
+const body = document.querySelector('body');
+const modeToggle = document.getElementById('mode-toggle');
+const modeStatus = document.querySelector('.mode-status');
+
+function toggleMode() {
+  body.classList.toggle('dark-mode');
+
+  const modeMessage = body.classList.contains('dark-mode') ?
+    'Dark Mode' 
+    : "Light Mode"
+}
+
+modeToggle.addEventListener('click', toggleMode);
